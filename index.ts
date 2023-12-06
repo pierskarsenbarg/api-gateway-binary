ad203f0bfb99361930bcb657375b89a4c1f1607b
Piers Karsenbarg <piers@pulumi.com>
2023-12-06 14:21:08 +0000

    First commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import * as aws from "@pulumi/aws";
+import * as apigateway from "@pulumi/aws-apigateway";
+
+const f = new aws.lambda.CallbackFunction("f", {
+    callback: async (ev, ctx) => {
+        console.log(JSON.stringify(ev));
+        return {
+            statusCode: 200,
+            body: "goodbye",
+        };
+    },
+});
+
+const api = new apigateway.RestAPI("pk-aws-api", {
+    binaryMediaTypes: [],
+    routes: [{
+        path: "/",
+        method: "GET",
+        eventHandler: f,
+    }],
+});
+
+export const apiId = api.api.id;
